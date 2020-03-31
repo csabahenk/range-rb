18fed33dee235bbbd1fa9d05d49a4f149707fdd9
Csaba Henk <csaba@lowlife.hu>
2020-03-31 10:36:47 +0200

    fix offset semantics
    As negative and positive indexing can occur
    simultaneously, we need to have a unified
    semantics for them. (Note: tail(1) uses 1-based
    indexing when passing `-n +<N>` to it but uses
    0-based indexing with `-n <N>`.)
    
    By default we use 0 based indexing but we offer
    now option `-o/--offset N` to shift the indexing.
    `-o1` might be more intuitive when only positve
    indexes occur.

--- range.rb
+++ range.rb
@@ -1,14 +1,19 @@
 #!/usr/bin/env ruby
 
+require File.expand_path "~/ruby/simpleopts/simpleopts.rb"
+
+so = SimpleOpts.get("<rangexp...>", offset: 0)
+conv = proc { |n| Integer(n) - so.offset }
+
 ra = $*.map { |a|
   a.split(/\s*,\s*/).map { |b|
     Range.new *case b
     when /\A(-?\d+)\Z/
-      Integer($1).then {|n| [n,n,false] }
+      conv[$1].then {|n| [n,n,false] }
     when /\A(-?\d+)(?:(?:\.{2}|-)|(\.{3}))(-?\d+)?\Z/
-      [$1,$3].map{|s| s ? Integer(s) : nil} + [!!$2]
+      [$1,$3].map{|s| s ? conv[s] : nil} + [!!$2]
     when /\A\.\.(\.)?(-?\d+)\Z/
-      [nil, Integer($2), !!$1]
+      [nil, conv[$2], !!$1]
     else raise ArgumentError, "cannot interpret #{b.inspect} as range"
     end
   }
@@ -18,11 +23,11 @@ has_neg = ra.find { |r| %i[begin end].find {|m| (r.send(m)||0) < 0 }}
 
 inp,ra = if has_neg
   lines = STDIN.readlines
-  [lines, (1..lines.size).to_a.then {|a| ra.map {|r| a[r] }}]
+  [lines, (0...lines.size).to_a.then {|a| ra.map {|r| a[r] }}]
 else
   [STDIN, ra]
 end
 
 inp.each_with_index { |l,i|
-  ra.find { |r| r.include? i+1 } and print l
+  ra.find { |r| r.include? i } and print l
 }
