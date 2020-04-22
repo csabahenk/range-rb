590d00b7adb170459eac4d1428da2fb94d330407
Csaba Henk <csaba@lowlife.hu>
2020-04-22 10:43:37 +0200

    support taking input from files given as argument

--- range.rb
+++ range.rb
@@ -7,7 +7,9 @@ so = SimpleOpts.get("<rangexp...>", offset: 0, number: false,
                     grep: SOpt.new(default: nil, type: Regexp))
 conv = proc { |n| Integer(n) - so.offset }
 
-ra = $*.map { |a|
+argv_orig = $*.dup
+$*.clear
+ra = argv_orig.map { |a|
   a.split(/\s*,\s*/).map { |b|
     Range.new *case b
     when /\A(-?\d+)\Z/
@@ -35,18 +37,20 @@ ra = $*.map { |a|
         proc { |v| [v,-1].min }
       end)
       ends + [false]
-    else raise ArgumentError, "cannot interpret #{b.inspect} as range"
+    else
+      $* << a
+      break
     end
   }
-}.flatten
+}.flatten.compact
 
 has_neg = ra.find { |r| %i[begin end].find {|m| (r.send(m)||0) < 0 }}
 
 inp,ra = if has_neg
-  lines = STDIN.readlines
+  lines = $<.readlines
   [lines, (0...lines.size).to_a.then {|a| ra.map {|r| a[r] }}]
 else
-  [STDIN, ra]
+  [$<, ra]
 end
 
 annotate = if so.number
